92e8ed4c66ed1b91b3dc648e3ac0a20bf3e0a417
bigapple520520 <bigapple520520@163.com>
2016-02-23 20:28:32 +0800

    1、修改调试会出现两个APP问题，升级到2.0.1

---
+++ bigappleuilib/src/main/java/com/xuan/bigappleui/lib/BigappleUI.java
@@ -0,0 +1,9 @@
+package com.xuan.bigappleui.lib;
+
+/**
+ * Bigapple框架
+ *
+ * Created by xuan on 16/2/23.
+ */
+public class BigappleUI {
+}
--- bigappleuilib/src/main/java/com/xuan/bigappleui/lib/MainActivity.java
+++
@@ -1,9 +0,0 @@
-package com.xuan.bigappleui.lib;
-
-import android.app.Activity;
-
-/**
- * Created by xuan on 16/2/22.
- */
-public class MainActivity extends Activity {
-}
